37fdfd1115e9ac3c0e85452b84d3f62dd26fc3dc
Andrea Strömberg <andreastromberg@yahoo.se>
2026-03-12 16:31:09 +0100

    Gjort färgpalett

--- functions.php
+++ functions.php
@@ -7,6 +7,7 @@
 function uv_script_and_styles()
 {
     wp_enqueue_style('main-style', get_stylesheet_directory_uri() . '/style.css', array(), filemtime(get_stylesheet_directory() . '/style.css'));
+    add_editor_style('style.css');
 }
 
 add_action('enqueue_block_assets', 'uv_script_and_styles');
